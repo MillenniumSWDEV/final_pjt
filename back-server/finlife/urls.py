4079beafa763f06dc4d05e661790ef1fb9cd2c2a
HJ <huhwo00@gmail.com>
2023-05-19 17:32:00 +0900

    cleanup:[BE] 오타수정

--- back-server/finlife/urls.py
+++ back-server/finlife/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('deposit-products-options/<str:fin_prdt_cd>/', views.deposit_product_options),         # 특정 상품의 옵션 리스트 출력
     path('deposit-products/top-rate/', views.top_rate), # 가입 기간에 상관없이 최고 금리가 가장 높은 금융 상품과 해당 상품의 옵션 리스트 출력
     path('deposit-product/<str:fin_prdt_cd>/', views.deposit_product),   # 상품 하나의 데이터.
-    path('save-savings-products/', views.save_savings_products),
+    path('save-saving-products/', views.save_savings_products),
     path('saving-products/', views.saving_products), # 전체 적금 상품 목록 출력 & 데이터 삽입
     path('save-ex-rate/', views.save_ex_rate), # 환율정보저장
     path('deposit-product/cart/<str:fin_prdt_cd>/', views.deposit_product_cart), # 예금상품 장바구니추가
