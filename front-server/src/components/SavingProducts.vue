c0b56abb3545a3e80a57096cda5228754abbb26f
shin <467shin@gmail.com>
2023-05-19 11:18:07 +0900

    feat:[FE] 예금 적금 정보 받아오기 완료

---
+++ front-server/src/components/SavingProducts.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h4>저끔저끔~!</h4>
+  <div v-for="sp, idx in this.savingProducts" v-bind:key="idx">
+        {{ idx+1 }} - {{ sp.fin_prdt_nm }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SavingProducts',
+    computed: {
+    savingProducts() {
+      return this.$store.state.savingProducts;
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
