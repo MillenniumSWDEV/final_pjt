41b7f63af5415c8943981eebd3ebd60ad95f880d
shin <467shin@gmail.com>
2023-05-19 09:17:20 +0900

    feat:[FE] 환율 포함 컴포넌트 전부 완성

---
+++ front-server/src/views/ExchangeRateView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>환율</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ExchangeRateView',
+    created() {
+        this.getExRate()
+    },
+    methods: {
+        getExRate() {
+            this.$store.dispatch('getExRate')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
