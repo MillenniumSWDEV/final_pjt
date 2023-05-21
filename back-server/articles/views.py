6a8a882b6d354d61a16d6c8cb98304303daa2ff4
허재 <huhwo00@gmail.com>
2023-05-21 17:45:32 +0900

    feat:signup기능 구현

--- back-server/articles/views.py
+++ back-server/articles/views.py
@@ -5,8 +5,8 @@ from rest_framework.decorators import api_view
 # from rest_framework.decorators import authentication_classes
 
 # permission Decorators
-# from rest_framework.decorators import permission_classes
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework import status
 from django.shortcuts import get_object_or_404, get_list_or_404
