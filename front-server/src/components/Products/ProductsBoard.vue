928add44b6a318db44be089c69f77e70761a16b8
CHAERYEON-LEE <sogogijjang@naver.com>
2023-05-23 15:48:15 +0900

    feat: [FE] 온라인 상품 페이지 구현완료

--- front-server/src/components/Products/ProductsBoard.vue
+++ front-server/src/components/Products/ProductsBoard.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>
-    <div class="container-lg">
+  <div id="ProductsBoard">
+    <b-nav class="container mt-3" tabs fill>
+      <b-nav-item
+        :active="deposit"
+        style="font-weight: 900; font-size: 22px"
+        @click="depositClick()"
+        >온라인 예금상품</b-nav-item
+      >
+      <b-nav-item
+        :active="saving"
+        style="font-weight: 900; font-size: 22px"
+        @click="savingClick()"
+        >온라인 적금상품</b-nav-item
+      >
+    </b-nav>
+    <div v-if="deposit" class="mt-3 container">
       <DepositProducts />
     </div>
-    <div class="container">
+    <div v-if="saving" class="mt-3 container">
       <SavingProducts />
     </div>
   </div>
@@ -14,10 +28,16 @@ import DepositProducts from "./DepositProducts.vue";
 import SavingProducts from "./SavingProducts.vue";
 
 export default {
-  name: "ProductsView",
+  name: "ProductsBoard",
   components: {
     DepositProducts,
     SavingProducts,
+  },
+  data() {
+    return {
+      deposit: true,
+      saving: false,
+    };
   },
   created() {
     this.getDepositProducts();
@@ -29,6 +49,18 @@ export default {
     },
     getSavingProducts() {
       this.$store.dispatch("getSavingProducts");
+    },
+    depositClick() {
+      this.deposit = true;
+      if (this.deposit) {
+        this.saving = false;
+      }
+    },
+    savingClick() {
+      this.saving = true;
+      if (this.saving) {
+        this.deposit = false;
+      }
     },
   },
 };
--- front-server/src/components/Products/SavingProducts.vue
+++ front-server/src/components/Products/SavingProducts.vue
@@ -1,43 +1,71 @@
 <template>
-  <div>
-    <h4>온라인 적금상품 [총 {{ savingProducts.length }} 건]</h4>
+  <div id="SavingProducts" style="width: 100%">
+    <h5>[총 {{ totalRows }} 건]</h5>
     <hr />
-    <b-card v-for="(sp, idx) in savingProducts" :key="idx">
-      <div class="d-flex justify-content-start align-self-center p-2">
-        <b-img
-          src="./moneyIcon.png"
-          width="200px"
-          height="200px"
-          rounded="circle"
-          alt="Rounded image"
-          class="my-auto"
-        ></b-img>
-        <div class="d-flex flex-column my-auto p-5">
-          <b-card-text>{{ sp.join_way.split(",").join(" | ") }}</b-card-text>
-          <h4>
-            <b-card-text> {{ sp.fin_prdt_nm }}/{{ sp.kor_co_nm }} </b-card-text>
-          </h4>
-          <b-card-text>{{ sp.spcl_cnd }}</b-card-text>
-          <div>
-            <b-button variant="info">가입하기</b-button>
-            <b-button
-              class="mx-3"
-              @click="goDetailPage({ id: sp.id, data: sp }, event)"
-              >상세보기</b-button
-            >
+    <div style="height: 680px">
+      <b-card v-for="(sp, idx) in savingProducts" :key="idx">
+        <div class="d-flex justify-content-start align-self-center px-2">
+          <div class="my-auto mx-auto" style="width: 150px">
+            <b-img
+              src="./moneyIcon.png"
+              width="100px"
+              height="100px"
+              rounded="circle"
+              alt="Rounded image"
+            ></b-img>
+          </div>
+          <div class="d-flex flex-column" style="width: 100%">
+            <b-card-text>{{ sp.join_way.split(",").join(" | ") }}</b-card-text>
+            <h5 style="font-weight: 900">
+              <b-card-text>
+                {{ sp.fin_prdt_nm }}/{{ sp.kor_co_nm }}
+              </b-card-text>
+            </h5>
+            <b-card-text>{{ sp.spcl_cnd }}</b-card-text>
+            <div>
+              <b-button variant="info">가입하기</b-button>
+              <b-button
+                class="mx-3"
+                @click="goDetailPage({ id: sp.id, data: sp }, event)"
+                >상세보기</b-button
+              >
+            </div>
           </div>
         </div>
-      </div>
-    </b-card>
+      </b-card>
+    </div>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="totalRows"
+      :per-page="perPage"
+      first-number
+      last-number
+      style="justify-content: center"
+    ></b-pagination>
   </div>
 </template>
 
 <script>
 export default {
   name: "SavingProducts",
+  data() {
+    return {
+      perPage: 3,
+      currentPage: 1,
+      items: [],
+    };
+  },
   computed: {
     savingProducts() {
-      return this.$store.state.finlife.savingProducts;
+      const items = this.$store.state.finlife.savingProducts;
+
+      return items.slice(
+        (this.currentPage - 1) * this.perPage,
+        this.currentPage * this.perPage
+      );
+    },
+    totalRows() {
+      return this.$store.state.finlife.savingProducts.length;
     },
   },
   methods: {
